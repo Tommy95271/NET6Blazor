41655b67ad047295fe7a6efcb6140ae17dc9f16e
Tommy <Tommy@miniasp.com>
2023-03-14 18:03:36 +0800

    JavaScript initializers

--- BlazorWebAssembly/Client/Program.cs
+++ BlazorWebAssembly/Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorWebAssembly.Client;
+using BlazorWebAssembly.Client.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.RootComponents.RegisterForJavaScript<Quote>(identifier: "quote",
+    javaScriptInitializer: "initializeComponent");
 
 await builder.Build().RunAsync();
---
+++ BlazorWebAssembly/Client/wwwroot/BlazorWebAssembly.Client.lib.module.js
@@ -0,0 +1,8 @@
+﻿export function afterStarted(blazor) {
+    let targetElement = document.getElementById('quoteContainer2');
+    blazor.rootComponents.add(targetElement, 'quote',
+        {
+            text: "Crow: I have my doubts that this movie is actually 'starring' " +
+                "anybody. More like, 'camera is generally pointed at.'"
+        });
+}
