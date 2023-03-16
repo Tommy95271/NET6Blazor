65f0bb1dd49a7f94640ddb95fc19bdb9a56087f2
Tommy <Tommy@miniasp.com>
2023-03-16 11:26:00 +0800

    Dynamic component example with parameter

---
+++ BlazorWebAssembly/Client/Models/ComponentMetadata.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorWebAssembly.Client.Models
+{
+    public class ComponentMetadata
+    {
+        public string? Name { get; set; }
+        public Dictionary<string, object> Parameters { get; set; } = new Dictionary<string, object>();
+    }
+}
